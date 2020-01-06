53214dcbdb3fafc6cd8e5e89b7293096b03d15d5
root <pineapplebo@yahoo.com>
2020-01-05 21:08:18 -0500

    this is my second commit

--- talktome.sh
+++ talktome.sh
@@ -5,4 +5,7 @@ echo "This is my talking program."
 echo -n "What is your name? "
 read name 
 echo "your name is $name"
+echo "What is your favorite color?"
+read color 
+echo "Nice! I see that your favorite color is $color"
 
